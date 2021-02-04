0d61fe72cb8fb8792b48bb5f3ce50af647eb2c1a
mathewhooper <67506290+mathewhooper@users.noreply.github.com>
2021-02-04 13:19:20 +0000

    Update setup_k6.sh

--- setup_k6.sh
+++ setup_k6.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
 
-apt-get update
+sudo apt-get update
 apt-get install dirmngr --install-recommends
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 379CE192D401AB61
 echo "deb https://dl.bintray.com/loadimpact/deb stable main" | sudo tee -a /etc/apt/sources.list
